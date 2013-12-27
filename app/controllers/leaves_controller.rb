f099ecccd25c119fc197bbd4e72caf1f2a2223fd
mercury <mercury@alphamelon.com>
2013-12-27 16:39:44 +0800

    added change date logic for calendar

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -10,11 +10,26 @@ class LeavesController < ApplicationController
     @approved_leaves = @organization.leaves.where(status: "Approved")
     @rejected_leaves = @organization.leaves.where(status: "Rejected")
     
-    @month = !params[:month].blank? ? params[:month] : DateTime.now.month
-    @year = !params[:year].blank? ? params[:year] : DateTime.now.year
+    @month = !params[:date][:month].blank? ? params[:date][:month].to_i : DateTime.now.month
+    @year = !params[:date][:year].blank? ? params[:date][:year].to_i : DateTime.now.year
     @date = DateTime.new(@year, @month)
+    @month_collection = [
+    ["January", 1], 
+    ["February", 2], 
+    ["March", 3], 
+    ["April", 4], 
+    ["May", 5], 
+    ["June", 6], 
+    ["July", 7], 
+    ["August", 8], 
+    ["September", 9], 
+    ["October", 10], 
+    ["November", 11], 
+    ["December", 12]]
     
     @employees = current_organization.employees.order(:id)
+    
+    #raise params.inspect
   end
   
   def new

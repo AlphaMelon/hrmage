cca3a7f41ac79ca14cfa72182148efcae365349b
Haziq Hafizuddin <haziq@alphamelon.com>
2014-01-15 13:44:43 +0800

    added send email for issue #27

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,33 @@
+class UserMailer < ActionMailer::Base
+  default from: "admin@melon.officemage.dev"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.signup_confirmation.subject
+  #
+  def signup_confirmation(user)
+    @user = user
+    mail to: user.email, subject: "Sign Up Confirmation"
+  end
+
+  def claim_approval(user)
+    @user = user
+    mail to: user.email, subject: "Claim Approval"    
+  end
+
+  def claim_need_approve(user)
+    @user = user
+    mail to: user.email, subject: "Claim need approval"    
+  end
+
+  def leave_approval(user)
+    @user = user
+    mail to: user.email, subject: "Leave Approval"    
+  end
+
+  def leave_need_approve(user)
+    @user = user
+    mail to: user.email, subject: "Leave need approval"    
+  end
+end

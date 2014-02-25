ef338c93683df053077b43d2aea0da3fabca00bd
Haziq Hafizuddin <haziq@alphamelon.com>
2014-02-25 11:41:38 +0800

    added style for issue #72

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.where(organization_id: current_organization.id).order(created_at: :desc)
+    @activities = PublicActivity::Activity.where(organization_id: current_organization.id).order(created_at: :desc).page(params[:page]).per(8)
   end
 end

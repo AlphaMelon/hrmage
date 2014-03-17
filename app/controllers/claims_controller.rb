96ed9009b9210c00e1f5f4b058721848a39c6cc1
Haziq Hafizuddin <haziq@alphamelon.com>
2014-03-17 11:16:36 +0800

    fix for issue #79

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -20,7 +20,6 @@ class ClaimsController < ApplicationController
     respond_to do |format|
       format.html
       format.csv { send_data @claims.to_csv }
-      format.xls # { send_data @claims.to_csv(col_sep: "\t") }
     end
   end
   

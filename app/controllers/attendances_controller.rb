ba89386e082d9fa7d5bee82b1eec748c9786ed8c
mercury <mercury@alphamelon.com>
2014-04-14 17:07:37 +0800

    added calendar in attendance

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -9,6 +9,24 @@ class AttendancesController < ApplicationController
   def index
     @attendances = current_organization.attendances.order(clock_time: :asc)
     @attendance = current_organization.attendances.new
+    @employees = current_organization.employees.order(id: :asc)
+    
+    @month = !params[:date].blank? ? params[:date][:month].to_i : DateTime.now.month
+    @year = !params[:date].blank? ? params[:date][:year].to_i : DateTime.now.year
+    @date = DateTime.new(@year, @month)
+    @month_collection = [
+    ["January", 1], 
+    ["February", 2], 
+    ["March", 3], 
+    ["April", 4], 
+    ["May", 5], 
+    ["June", 6], 
+    ["July", 7], 
+    ["August", 8], 
+    ["September", 9], 
+    ["October", 10], 
+    ["November", 11], 
+    ["December", 12]]
     
   end
   
@@ -19,16 +37,21 @@ class AttendancesController < ApplicationController
   def create
     csv_text = File.read(params[:attendance_file].tempfile.to_path.to_s)
     csv = CSV.parse(csv_text, :headers => false)
+    count = 0
     csv.each do |row|
-      raise row.inspect
+      employee = current_organization.employees.where(employee_identification: row[03]).first
+      if !employee.blank?
+        date_str = "20#{row[01][4..5]}-#{row[01][2..3]}-#{row[01][0..1]} #{row[02][0..1]}:#{row[02][2..3]}"
+        attendance = employee.attendances.new
+        attendance.organization_id = current_organization.id
+        attendance.clock_time = date_str.to_datetime
+        attendance.save
+        count = count + 1 if attendance.save
+      end
     end
     
-    @attendance = current_organization.attendances.new(attendance_params)
-    if @attendance.save
-      redirect_to organization_attendances_path(current_organization), notice: "Attendance successfully created"
-    else
-      render action: 'new'
-    end
+    redirect_to organization_attendances_path(current_organization), notice: "#{count} Attendance successfully created"
+
   end
   
   def edit
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,12 @@
 class Attendance < ActiveRecord::Base
   belongs_to :organization
   belongs_to :employee
+
+  validates :clock_time, presence: true
+  validates :organization, presence: true
+  validates :employee_id, presence: true
 end
+
+
+
+
